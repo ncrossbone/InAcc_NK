602edf18aa09129689264ec5f21b74213b715391
hyeokpark92 <hyeokpark92@gmail.com>
2016-12-07 14:12:03 +0900

    hyeok-commit

--- app/view/map/CoreMap.js
+++ app/view/map/CoreMap.js
@@ -28,8 +28,8 @@ Ext.define('InAcc.view.map.CoreMap', {
         			source: new ol.source.OSM()
         		})],
         		view: new ol.View({
-        			center: ol.proj.fromLonLat([37.41, 8.82]),
-        			zoom: 4
+        			center: ol.proj.fromLonLat([127, 36]),
+        			zoom: 8
         		})
         	});
         	
